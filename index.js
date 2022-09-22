1b0061a5ab953e9345b8f79f9db8be3486bb7e50
Alelsito <lbalelxander@gmail.com>
2022-09-21 22:52:27 -0500

    ✨ KATA 2: SUBS

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ function sum (a, b) {
   return a + b
 }
 
-console.log('La suma de a y b es igual a: ' + sum(1, 2))
+console.log(`La suma de a y b es igual a: ${sum(1, 2)}`)
+
+// Ejercicio 2
+/* Crea una función llamada subs que reste dos parametros "a" y "b" y regrese su resultado. Recuerda que para devolver el valor de una función se utiliza la palabra reservada return */
+
+function subs (a, b) {
+  return a - b
+}
+
+console.log(`La resta de a y b es igual a: ${subs(50, 28.5)}`)
