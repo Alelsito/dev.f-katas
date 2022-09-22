b84823e3676d7819f3d8ad18abc8dca1361ee51a
Alelsito <lbalelxander@gmail.com>
2022-09-21 22:48:30 -0500

    ✨ Kata 1: SUM

---
+++ index.js
@@ -0,0 +1,8 @@
+// Ejercicio 1
+/* Crea una función llamada sum que reciba dos parametros "a" y "b" y regrese la suma de ambos números. Recuerda que para devolver el valor de una función se utiliza la palabra reservada return */
+
+function sum (a, b) {
+  return a + b
+}
+
+console.log('La suma de a y b es igual a: ' + sum(1, 2))
