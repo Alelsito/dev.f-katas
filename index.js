65681c7ff45c11385b0d4d825a63ce2df648c27b
Alelsito <lbalelxander@gmail.com>
2022-09-21 22:56:17 -0500

    ✨ Kata 3: VARIABLES

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 // Ejercicio 1
 /* Crea una función llamada sum que reciba dos parametros "a" y "b" y regrese la suma de ambos números. Recuerda que para devolver el valor de una función se utiliza la palabra reservada return */
 
-function sum (a, b) {
-  return a + b
-}
+// function sum (a, b) {
+//   return a + b
+// }
 
-console.log(`La suma de a y b es igual a: ${sum(1, 2)}`)
+// console.log(`La suma de a y b es igual a: ${sum(1, 2)}`)
 
 // Ejercicio 2
 /* Crea una función llamada subs que reste dos parametros "a" y "b" y regrese su resultado. Recuerda que para devolver el valor de una función se utiliza la palabra reservada return */
 
-function subs (a, b) {
-  return a - b
-}
+// function subs (a, b) {
+//   return a - b
+// }
 
-console.log(`La resta de a y b es igual a: ${subs(50, 28.5)}`)
+// console.log(`La resta de a y b es igual a: ${subs(50, 28.5)}`)
+
+// Ejercicio 3
+/* Crea una variable llamada cont que sea del tipo entero, después crea otra variable llamada bool que sea del tipo booleano y por último una variable llamada str que sea del tipo string. Recuerda utilizar la palabra reservada var para este ejercicio. */
+
+const cont = 8
+const bool = true
+const str = 'perro'
+
+console.log(`La variable cont es igual a: ${cont}`)
+console.log(`La variable bool es igual a: ${bool}`)
+console.log(`La variable str es igual a: ${str}`)
