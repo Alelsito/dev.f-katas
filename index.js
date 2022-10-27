a40b9688c6c3f54ed581891f8c4a15a9644a90b7
Alelsito <lbalelxander@gmail.com>
2022-10-27 12:13:00 -0500

    feat: Kata 36 (SHOWMESSAJE)

--- index.js
+++ index.js
@@ -734,21 +734,37 @@
 Crea una funcion llamada petNames() A partir del siguiente arreglo de mascotas: const mascotas = [ { nombre: "Bimbo", especie: "Hamster", comidasFavoritas: ["Semillas", "Nueces"] }, { nombre: "Ludovico", especie: "Perro", comidasFavoritas: ["Huesos", "Jamon"] }, { nombre: "Pavlov", especie: "Perro", comidasFavoritas: ["Croquetas", "Pollo"] }, { nombre: "Chancla", especie: "Gato", comidasFavoritas: ["Atun", "Pescado"] }, { nombre: "Don Pepe", especie: "Perico", comidasFavoritas: ["Semillas"] } ]; Genera un nuevo arreglo que contenga los nombres de todas las mascotas.
 */
 
-const mascotas = [
-  { nombre: 'Bimbo', especie: 'Hamster', comidasFavoritas: ['Semillas', 'Nueces'] },
-  { nombre: 'Ludovico', especie: 'Perro', comidasFavoritas: ['Huesos', 'Jamon'] },
-  { nombre: 'Pavlov', especie: 'Perro', comidasFavoritas: ['Croquetas', 'Pollo'] },
-  { nombre: 'Chancla', especie: 'Gato', comidasFavoritas: ['Atun', 'Pescado'] },
-  { nombre: 'Don Pepe', especie: 'Perico', comidasFavoritas: ['Semillas'] }
-]
+// const mascotas = [
+//   { nombre: 'Bimbo', especie: 'Hamster', comidasFavoritas: ['Semillas', 'Nueces'] },
+//   { nombre: 'Ludovico', especie: 'Perro', comidasFavoritas: ['Huesos', 'Jamon'] },
+//   { nombre: 'Pavlov', especie: 'Perro', comidasFavoritas: ['Croquetas', 'Pollo'] },
+//   { nombre: 'Chancla', especie: 'Gato', comidasFavoritas: ['Atun', 'Pescado'] },
+//   { nombre: 'Don Pepe', especie: 'Perico', comidasFavoritas: ['Semillas'] }
+// ]
 
-function petNames (array) {
-  const mascotasName = []
-  array.map(m => {
-    const name = m.nombre
-    mascotasName.push(name)
-  })
-  return mascotasName
+// function petNames (array) {
+//   const mascotasName = []
+//   array.map(m => {
+//     const name = m.nombre
+//     mascotasName.push(name)
+//   })
+//   return mascotasName
+// }
+
+// console.log(petNames(mascotas))
+
+// Ejercicio 3️⃣6️⃣
+
+/*
+Crea una función llamada showMessaje() que devuelva "Este es mi mensaje dentro de un callback" mediante un callback.
+*/
+
+function callback (param) {
+  return param
 }
 
-console.log(petNames(mascotas))
+function showMessaje (call) {
+  return call('Este es mi mensaje dentro de un callback')
+}
+
+console.log(showMessaje(callback))
